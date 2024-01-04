b61a2415d1e5dcf374c91adee74f84dbd9b68a28
Colk-tech <master@colk.dev>
2024-01-04 18:31:41 +0900

    feat: add a new mixin for sqlalchemy, `TimeStampMixin`

---
+++ src/common/library/sqlalchemy/mixin/timestamp.py
@@ -0,0 +1,44 @@
+import datetime
+
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+DEFAULT_TIMEZONE: datetime.timezone = datetime.timezone.utc
+
+DEFAULT_CREATED_AT_COLUMN_NAME: str = "created_at"
+DEFAULT_UPDATED_AT_COLUMN_NAME: str = "updated_at"
+
+
+class CreatedAtMixin(object):
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        name=DEFAULT_CREATED_AT_COLUMN_NAME,
+        type_=DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+
+
+class UpdatedAtMixin(object):
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        name=DEFAULT_UPDATED_AT_COLUMN_NAME,
+        type_=DateTime(timezone=True),
+        onupdate=func.now(),
+        nullable=False,
+    )
+
+
+# We get an error when we do 'class TimestampMixin(CreatedAtMixin, UpdatedAtMixin)'
+class TimestampMixin(object):
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        name=DEFAULT_CREATED_AT_COLUMN_NAME,
+        type_=DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        name=DEFAULT_UPDATED_AT_COLUMN_NAME,
+        type_=DateTime(timezone=True),
+        onupdate=func.now(),
+        nullable=False,
+    )
