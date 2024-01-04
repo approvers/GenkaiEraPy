0fa3c1c6dfea121952732e7765efe8180c07c0a1
Colk-tech <master@colk.dev>
2024-01-04 18:39:16 +0900

    feat: add `Repository`

---
+++ src/adapter/repository/repository.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from src.adapter.session import AsyncSessionWrapperInterface
+from src.common.design.interface import Interface
+
+
+class Repository[RawSessionType](Interface):
+    __session_wrapper: Optional[AsyncSessionWrapperInterface[RawSessionType]] = None
+
+    def set_session_wrapper(
+        self,
+        session: AsyncSessionWrapperInterface[RawSessionType],
+    ) -> None:
+        self.__session_wrapper: AsyncSessionWrapperInterface[RawSessionType] = session
+
+    def get_session_wrapper(
+        self,
+    ) -> AsyncSessionWrapperInterface[RawSessionType]:
+        if self.__session_wrapper is None:
+            raise RuntimeError("'self.__session' is not set")
+
+        return self.__session_wrapper
+
+    def get_raw_session(
+        self,
+    ) -> RawSessionType:
+        return self.get_session_wrapper().get_raw_session()
