0276411de3d0d5286ccafb2a5d063476645e9481
Colk-tech <master@colk.dev>
2024-01-04 18:31:13 +0900

    feat: add a new mixin for sqlalchemy, `UIDMixin`

---
+++ src/common/library/sqlalchemy/mixin/uid.py
@@ -0,0 +1,17 @@
+import uuid
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.common.library.sqlalchemy.field.sauuid import SAUUID
+
+DEFAULT_UID_COLUMN_NAME: str = "uid"
+
+
+class UIDMixin(object):
+    uid: Mapped[uuid.UUID] = mapped_column(
+        name=DEFAULT_UID_COLUMN_NAME,
+        type_=SAUUID,
+        primary_key=True,
+        default=uuid.uuid4,
+        nullable=False,
+    )
