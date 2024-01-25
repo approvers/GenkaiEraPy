5e0a334f667f32cd504799ae3ac8e7a58eef9758
Colk-tech <master@colk.dev>
2024-01-26 00:10:18 +0900

    feat: add `Member` entity

---
+++ src/domain/member/entity.py
@@ -0,0 +1,15 @@
+from src.domain.base.entity import Entity
+from src.domain.common.value import NullableCreatedAt
+from src.domain.member.field import MemberLocalNickname
+from src.domain.server.value import ServerIdentifier
+from src.domain.user.field import UserIdentifier
+
+
+class Member(Entity):
+    created_at: NullableCreatedAt
+    updated_at: NullableCreatedAt
+
+    server_id: ServerIdentifier
+    user_id: UserIdentifier
+
+    local_nickname: MemberLocalNickname
