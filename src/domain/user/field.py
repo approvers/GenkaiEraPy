25b5a07036b5d12db4a7d050387fb9fac5db61c4
Colk-tech <master@colk.dev>
2024-01-25 23:36:51 +0900

    feat: add `description` in `.../user/field.py`

--- src/domain/user/field.py
+++ src/domain/user/field.py
@@ -1,9 +1,16 @@
+from pydantic import Field
+
 from src.domain.base.value import ValueObject
 
 
 class UserID(ValueObject):
-    value: str
+    value: str = Field(
+        description="ID of user",
+    )
 
 
 class UserNickname(ValueObject):
-    value: str
+    value: str = Field(
+        description="Nickname of user, which is displayed as user's name.\n"
+                    "Not unique like @username in X ( Twitter ).",
+    )
