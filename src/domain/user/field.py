6edcf786f770ec07fa83c8f825720b8573e6db0e
Colk-tech <master@colk.dev>
2024-01-25 23:41:19 +0900

    rename: rename `UserID` -> `UserIdentifier`

--- src/domain/user/field.py
+++ src/domain/user/field.py
@@ -3,9 +3,9 @@ from pydantic import Field
 from src.domain.base.value import ValueObject
 
 
-class UserID(ValueObject):
+class UserIdentifier(ValueObject):
     value: str = Field(
-        description="ID of user",
+        description="ID of user, must be unique and cannot be changed.",
     )
 
 
