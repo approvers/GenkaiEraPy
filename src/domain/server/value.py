4635f733b793ebc05fcf294e661d9f344e6d0fd4
Colk-tech <master@colk.dev>
2024-01-26 00:11:42 +0900

    feat: add a `ValueObject`: `ServerName`

--- src/domain/server/value.py
+++ src/domain/server/value.py
@@ -7,3 +7,10 @@ class ServerIdentifier(ValueObject):
     value: str = Field(
         description="ID of server, must be unique and cannot be changed.",
     )
+
+
+class ServerName(ValueObject):
+    value: str = Field(
+        description="Name of server, which is displayed as server's name.\n"
+        "Not unique like @username in X ( Twitter ).",
+    )
