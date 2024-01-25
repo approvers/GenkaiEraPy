04137284eac0ae849e097f5fe942f78de51442de
Colk-tech <master@colk.dev>
2024-01-26 00:02:39 +0900

    feat: add `created_at` and `updated_at` in `User`

--- src/domain/user/entity.py
+++ src/domain/user/entity.py
@@ -1,7 +1,11 @@
 from src.domain.base.entity import Entity
+from src.domain.common.value import NullableCreatedAt, NullableUpdatedAt
 from src.domain.user.field import UserIdentifier, UserNickname
 
 
 class User(Entity):
+    created_at: NullableCreatedAt
+    updated_at: NullableUpdatedAt
+
     identifier: UserIdentifier
     nickname: UserNickname
